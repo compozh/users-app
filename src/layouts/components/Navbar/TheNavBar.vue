52c09b7c4e281db5a6fce6b176dee2109920b696
Ihor Pievchev <pevchev@it-enterprise.com>
2020-09-22 14:57:52 +0300

    navbar created

---
+++ src/layouts/components/Navbar/TheNavBar.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app-bar app>
+    <v-toolbar-title class="d-flex align-center">
+      <v-img src="@/assets/love-road-logo.png" contain max-height="50px"/>
+      <span v-if="$vuetify.breakpoint.smAndUp" class="display-1">Road to Love</span>
+    </v-toolbar-title>
+    <v-spacer/>
+    <user-panel/>
+  </v-app-bar>
+</template>
+
+<script>
+import UserPanel from './UserPanel'
+
+export default {
+  name: 'TheNavBar',
+  components: {
+    UserPanel
+  }
+}
+</script>
---
+++ src/layouts/components/Navbar/UserPanel.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <span v-if="$vuetify.breakpoint.smAndUp" class="title">{{ userName() }}</span>
+    <v-avatar class="mx-3" color="primary" size="35" tile>
+      <v-icon color="white">mdi-account</v-icon>
+    </v-avatar>
+    <v-tooltip bottom>
+      <template #activator="{ on }">
+        <v-btn icon v-on="on"
+               @click="$store.dispatch('logout')">
+          <v-icon size="35">mdi-logout</v-icon>
+        </v-btn>
+      </template>
+      <span>Logout</span>
+    </v-tooltip>
+  </div>
+</template>
+
+<script>
+import users from '@/mixins/users'
+
+export default {
+  name: 'UserPanel',
+  mixins: [users],
+  computed: {
+    user () {
+      return this.$store.state.user
+    },
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
