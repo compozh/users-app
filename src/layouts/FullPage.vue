0926eac74dc75a9d944a2454fbe10565db067d83
Ihor Pievchev <pevchev@it-enterprise.com>
2020-09-22 14:58:08 +0300

    layouts created

---
+++ src/layouts/FullPage.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app>
+    <notify/>
+    <v-main class="layout--full-page">
+      <router-view/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Notify from '@/components/Notify'
+
+export default {
+  name: 'FullPage',
+  components: {
+    Notify
+  }
+}
+</script>
+
+<style scoped>
+
+  .layout--full-page {
+    background: url(../assets/love-road-bg.png) center center/cover no-repeat;
+  }
+
+</style>
---
+++ src/layouts/Main.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app>
+    <notify/>
+    <the-nav-bar/>
+    <v-main class="grey lighten-3">
+      <v-container fluid style="height: 100%"
+                   class="d-flex flex-column pa-2 pb-0">
+        <router-view/>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Notify from '@/components/Notify'
+import TheNavBar from './components/Navbar/TheNavBar'
+import users from '@/mixins/users'
+
+export default {
+  name: 'Main',
+  mixins: [users],
+  components: {
+    Notify,
+    TheNavBar
+  },
+  created () {
+    if (!this.user) {
+      this.$store.dispatch('setUserData')
+    }
+  }
+}
+</script>
